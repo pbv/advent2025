46af332c601b6f3993d564400e1a9548af0cf2c3
Pedro Vasconcelos <pbv@dcc.fc.up.pt>
2025-12-12 11:29:07 +0000

    Alternative solution to day 11

--- aoc11/Main.hs
+++ aoc11/Main.hs
@@ -18,7 +18,7 @@ main = do
   putStrLn "Part 1:"
   print (part1 inp)
   putStrLn "Part 2:"
-  print (part2 inp)
+  print (part2' inp)
 
 type Node = String
 type Input = Map Node [Node]
@@ -48,18 +48,18 @@ writeMemo node dist = do
   
 
 part1 :: Input -> Int
-part1 graph = evalState (dfs graph "you") Map.empty
+part1 graph = evalState (dfs graph "you" "out") Map.empty
 
-dfs :: Input -> Node -> State Memo Int
-dfs graph node 
-  | node == "out" =
+dfs :: Input -> Node -> Node -> State Memo Int
+dfs graph node end 
+  | node == end =
       return 1
   | otherwise = do
       optd <- lookupMemo node
       case optd of
         Just d -> return d
         Nothing -> do
-          dists <- sequence [ dfs graph node' 
+          dists <- sequence [ dfs graph node' end
                             | node' <- fromMaybe []
                                        (Map.lookup node graph)
                             ]
@@ -81,9 +81,22 @@ part2 graph = all - noDac - noFft + noDacFft
     graph1 = Map.delete "dac" graph
     graph2 = Map.delete "fft" graph
     graph3 = Map.delete "dac" graph2
-    all = evalState (dfs graph "svr") Map.empty
-    noDac = evalState (dfs graph1 "svr") Map.empty
-    noFft = evalState (dfs graph2 "svr") Map.empty
-    noDacFft =evalState (dfs graph3 "svr") Map.empty
+    all = evalState (dfs graph "svr" "out") Map.empty
+    noDac = evalState (dfs graph1 "svr" "out") Map.empty
+    noFft = evalState (dfs graph2 "svr" "out") Map.empty
+    noDacFft =evalState (dfs graph3 "svr" "out") Map.empty
       
 
+-- Alternative
+-- factor paths as svr -> dac -> fft -> out
+-- and svr -> fft -> dac -> out
+part2' :: Input -> Int
+part2' graph = paths1*paths2*paths3 + paths4*paths5*paths6
+  where
+    paths1 = evalState (dfs graph "svr" "dac") Map.empty
+    paths2 = evalState (dfs graph "dac" "fft") Map.empty
+    paths3 = evalState (dfs graph "fft" "out") Map.empty
+    paths4 = evalState (dfs graph "svr" "fft") Map.empty
+    paths5 = evalState (dfs graph "fft" "dac") Map.empty
+    paths6 = evalState (dfs graph "dac" "out") Map.empty
+    
